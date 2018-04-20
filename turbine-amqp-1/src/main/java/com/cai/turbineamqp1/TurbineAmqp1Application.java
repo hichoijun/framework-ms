2e725ea976c826de3b7a629ec92ecb09e4e9ef20
caijun <jun.cai@teamaxsc.com>
2018-04-20 19:28:33 +0800

    study

--- turbine-amqp-1/src/main/java/com/cai/turbineamqp1/TurbineAmqp1Application.java
+++ turbine-amqp-1/src/main/java/com/cai/turbineamqp1/TurbineAmqp1Application.java
@@ -2,6 +2,8 @@ package com.cai.turbineamqp1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @EnableTurbineStream
 @EnableDiscoveryClient
